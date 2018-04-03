0e0e34cfe1580181b4640bfe979af1352c8dd936
Suraj Kumar <suraj9693@outlook.com>
2018-04-04 00:37:06 +0530

    added something for checking

---
+++ src/test/java/com/qa/Finaltest/Test3.java
@@ -0,0 +1,18 @@
+package com.qa.Finaltest;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+@Test
+public void ETest(){
+	System.out.println("Just for demo1");
+}
+@Test
+public void FTest(){
+	System.out.println("for demo2");
+}
+@Test
+public void GTest(){
+	System.out.println("for demo3");
+}
+}
---
+++ src/test/java/com/qa/Finaltest/Test4.java
@@ -0,0 +1,10 @@
+package com.qa.Finaltest;
+
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void HTest(){
+		System.out.println("for demo4");
+	}
+}
